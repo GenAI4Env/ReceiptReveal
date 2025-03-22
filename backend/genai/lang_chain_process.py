0ef76b49302aae515a290b88b278e29728c4278e
digitalyoshixi <digitalyoshixi@proton.me>
2025-03-22 15:44:47 -0400

    new RAG

--- backend/genai/lang_chain_process.py
+++ backend/genai/lang_chain_process.py
@@ -6,8 +6,13 @@ import google.generativeai as genai
 from langchain_core.messages import HumanMessage
 from langchain_google_genai import ChatGoogleGenerativeAI, GoogleGenerativeAIEmbeddings
 from langchain.vectorstores import DocArrayInMemorySearch
+from langchain.schema.runnable import RunnableMap
+from langchain.prompts import ChatPromptTemplate
+from langchain.schema.output_parser import StrOutputParser
+from langchain_community.document_loaders.csv_loader import CSVLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 
-# -- loading env for api keys ---
+# --- loading env for api keys ---
 load_dotenv()
 
 # --- fetch the google gemini ---
@@ -17,12 +22,32 @@ llm = ChatGoogleGenerativeAI(model="gemini-2.0-flash", temperature=0.7)
 
 #  ------ setup RAG -----
 
+loader = CSVLoader(file_path="./genai/Food_Production.csv")
+data = loader.load()
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=100)
+docs = text_splitter.split_documents(data)
 embeddings = GoogleGenerativeAIEmbeddings(model = "models/embedding-001")
-vectorstore = DocArrayInMemorySearch.from_texts(
-        ["The answer to life, universe and everything is 43"],
+vectorstore = DocArrayInMemorySearch.from_documents(
+        docs,
         embedding=embeddings
 )
+
 # making the RAG
 retriever = vectorstore.as_retriever()
-print(retriever.get_relevant_documents("What is the answer to life universe and everything?"))
+#print(retriever.get_relevant_documents("Who is the current CEO of microsoft?"))
+# --- Making chain ---
+# setting up the format for output and the specific prompt engineering 
+template ="""answer based off this context: {context}
+in the json format:
+{{"answer"}} : "..."
+for this question: {question}
+"""
+prompt = ChatPromptTemplate.from_template(template)
+output_parser = StrOutputParser()
+# creating chain
+chain = RunnableMap({
+    "context" : lambda x : retriever.get_relevant_documents(x["question"]),
+    "question" : lambda x : x["question"]
+    }) | prompt | llm | output_parser
 
+print(chain.invoke({"question" : "what is the carbon footprint of eggs"}))
