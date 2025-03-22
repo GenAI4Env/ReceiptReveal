f9b3e8f256f108c174f2a16929ca6ef16c827ba2
digitalyoshixi <digitalyoshixi@proton.me>
2025-03-22 01:33:41 -0400

    gemini text ad image

--- machine-learning/gemini_handler.py
+++ machine-learning/gemini_handler.py
@@ -1,1 +1,20 @@
+# app.py
+from dotenv import load_dotenv
+import os
+load_dotenv()
+import google.generativeai as genai
+from PIL import Image, ImageFile
 
+genai.configure(api_key=os.environ["GEMINI_API_KEY"])
+
+model = genai.GenerativeModel('gemini-2.0-flash')
+
+def image_resp(prompt : str, image : ImageFile) -> str:
+    response = model.generate_content([prompt, image])
+    return response.text
+
+def text_resp(prompt : str) -> str:
+    response = model.generate_content(prompt)
+    return response.text
+
+print(text_resp("who is up tonight?"))
